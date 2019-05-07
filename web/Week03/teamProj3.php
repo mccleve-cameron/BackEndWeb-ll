78bc22d18a249dbc1637060486c1d30e2ee6720b
cameronmccleve <cameronmccleve@gmail.com>
2019-05-07 16:49:20 -0600

    week03 team project

--- web/Week03/teamProj3.php
+++ web/Week03/teamProj3.php
@@ -1,48 +1,40 @@
 <!DOCTYPE html>
 <html>
 
-
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="teamProj3.css">
 </head>
 
-<header>Confirm Purchase</header>
+<header>Here is Your Info</header>
 
 <body>
 
 	<h2>Please Review Your Purchase;</h2>
     
-<?php
+    <?php
 
-$name = $_POST['Name'];
-$email = $_POST['email'];
-$major = $_POST['major'];
-$comment = $_POST['comment']
-$continents = $_POST["continent"];
+        $name = $_POST['Name'];
+        $email = $_POST['email'];
+        $major = $_POST['major'];
+        $comment = $_POST['comment']
+        $continents = $_POST["continent"];
 
-		echo "Name is: ".$name. "<br/>";		
-		echo "Email is: ".$email. "<br/>";
+        echo "Name is: ".$name. "<br/>";		
+        echo "Email is: ".$email. "<br/>";
         echo "Major is: " .$major. "<br/>";
-		
+        
 
 
-		echo "<h3>Continents you've been to!</h3>";
-	
-        foreach($continents as $continent) { 
-            $continent_clean = htmlspecialchars($continent);
-        echo "<li><p>$continent_clean</p><?li>";
-        }
-
-
-		?>
+        echo "<h3>Continents you've been to!</h3>";
+    
+       // foreach($continents as $continent) { 
+         //   $continent_clean = htmlspecialchars($continent);
+           // echo "<li><p>$continent_clean</p><?li>";
+        //}
+    ?>
     
 </body>
-
-		<form action="assign11_a.php" method="post">
-        <div><input type="submit" name="review" value="Confirm"> </div>
-        <div><input type="submit" name="review" value="Cancel"></div>
-		</form>
 
 <footer>END</footer>
 
