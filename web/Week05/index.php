ee28eb9c68d443b1deccfce10320c3276c9b1fee
cameronmccleve <cameronmccleve@gmail.com>
2019-05-23 15:38:46 -0600

    week05

--- web/Week05/index.php
+++ web/Week05/index.php
@@ -2,11 +2,11 @@
 
     try
     {
-        $hostname = "insert values here";
-        $username = "insert values here";
-        $password = "insert values here";
-        $port     = "insert values here";
-        $database = "insert values here";
+        $hostname = "ec2-54-227-245-146.compute-1.amazonaws.com";
+        $username = "xishlqtlnqqhqr";
+        $password = "a5599158f6b6bba85cd781c336281b6ac6e2d64a10097e5cec6281617ab2fc52";
+        $port     = "5432";
+        $database = "d54ldp8o2hi2p6";
 
         $db = new PDO("pgsql:host=$hostname;port=$port;dbname=$database", $username, $password);
     }
