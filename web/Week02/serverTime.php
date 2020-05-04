5af9439397222aef6afdd535df21619defe64270
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-05-04 17:18:42 -0600

    homepage

--- web/Week02/serverTime.php
+++ web/Week02/serverTime.php
@@ -1,1 +1,32 @@
 <?php echo date("D M d, Y G:i a"); ?>
+
+<head>
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="homePage.css">
+</head>
+
+
+    <div class="nav">
+        <ul>
+            <li><a href="homePage.html">HOME</a></li>
+            <li><a href="assignments.html">ASSIGNMENTS</a></li>
+            <li><a href="serverTime.php">TIME</a></li>
+        </ul>
+    </div>
+
+<body>
+    <div>
+        <?php echo date("D M d, Y G:i a"); ?>
+    </div>
+
+    
+
+
+
+    <footer>
+        <div>
+
+        </div>
+        &copy; Cameron McCleve</footer>
+
+</body>   
