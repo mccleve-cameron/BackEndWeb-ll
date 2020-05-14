69f4ffdd5aab7af881a6ac8c995af1db641a65f8
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-05-14 15:24:36 -0600

    cart

--- web/browse.php
+++ web/browse.php
@@ -5,12 +5,9 @@ if (empty($_SESSION['cart'])) {
     //$_SESSION['cart'] = array();
 }
 
-if (isset($_POST['hat']))
-{
-    $_SESSION['hat'] = "hat";
-}
-
-echo $_SESSION['hat'];
+if (isset($_POST['hat'])) {$_SESSION['hat'] = "hat";}
+if (isset($_POST['shirt'])) {$_SESSION['shirt'] = "shirt";}
+if (isset($_POST['shoes'])) {$_SESSION['shoes'] = "shoes";}
 
 ?>
 
@@ -19,34 +16,43 @@ echo $_SESSION['hat'];
 
 <head>
     <meta charset="utf-8">
-    <script src="cart.js" ></script>
+    <script src="cart.js"></script>
     <link rel="stylesheet" href="practice.css">
     <title>Browse</title>
 </head>
 
 <header class="softRed">Browse Page</header>
-    
+
 <body>
 
-<main>
-    <section>
-        <div>Hat</div>
-        <div><form method="POST" action="">
-            <button type="submit" name="hat" value="hat">Add to Cart</button>
-        </form>
-</div>
-    </section>
-    <section>
-        <div>Shirt</div>
-        <div><button>Add to Cart</button></div>
-    </section>
-    <section>
-        <div>Shoes</div>
-        <div><button>Add to Cart</button></div>
-    </section>
+    <main>
+        <section>
+            <div>Hat</div>
+            <div>
+                <form method="POST" action="">
+                    <button type="submit" name="hat" value="hat">Add to Cart</button>
+                </form>
+            </div>
+        </section>
+        <section>
+            <div>Shirt</div>
+            <div>
+                <form method="POST" action="">
+                    <button type="submit" name="shirt" value="shirt">Add to Cart</button>
+                </form>
+            </div>
+        </section>
+        <section>
+            <div>Shoes</div>
+            <div>
+                <form method="POST" action="">
+                    <button type="submit" name="shoes" value="shoes">Add to Cart</button>
+                </form>
+            </div>
+        </section>
 
-    <button><a href="cart.php">View Cart</a></button>
-</main>
+        <button><a href="cart.php">View Cart</a></button>
+    </main>
 
 
     <footer class="softRed">
