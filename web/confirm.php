2cc82b12163958a2f1cc1fd6d0268a85108c5350
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-05-14 17:11:45 -0600

    cart

--- web/confirm.php
+++ web/confirm.php
@@ -20,7 +20,15 @@ echo $_SESSION['address'];
 
 <header class="softRed">Thanks for Your Purchase!</header>
 <body>
-    <h3>Your order will be shipped to:</h3>
+    <h3>Your items...</h3>
+    <ul>
+        <?php
+        foreach($_SESSION as $Key => $Value) {
+            echo "<li>$Value</li>";
+        }
+        ?>
+    </ul>
+    <h3>...will be shipped to:</h3>
     <?php echo $_SESSION['address']; ?>
 </body>
 
