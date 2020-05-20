b9cd67db477d6a36f96b49638785c59c3c825c0f
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-05-20 17:02:45 -0600

    week5team

--- web/Week05/team5/stretch2.php
+++ web/Week05/team5/stretch2.php
@@ -8,7 +8,7 @@ $db = get_db();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Stretch #2</title>
     <link rel="stylesheet" href="team5.css">
 </head>
 <header><h1>Stretch Challenge #2</h1></header>
