ec532ccf43fda9d304202f39ea0fca7a63e84d6c
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-05-28 16:24:22 -0600

    week5

--- web/Week05/dataAccess/data.php
+++ web/Week05/dataAccess/data.php
@@ -1,3 +1,8 @@
+<?php
+    session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,18 +14,17 @@
 <body>
 <h3>Search for a Username</h3>
 <form method="GET" action="display.php">
-<button type="submit" id="search">Search</button>
-    <input id="uInput" type="search" placeholder="username">
+<button type="submit" id="search"  name="search">Search</button>
+    <input id="uInput" type="search" name="uInput" placeholder="username">
 </form>
 
+<script>
+    const username = document.querySelector("#search").addEventListener('click', display);
 
-        <script>
-        const username = document.querySelector("#search").addEventListener('click', display);
-
-        function display() {
-            const username = document.getElementById('uInput').value;
-            console.log(username);
-        }
-    </script>
+    function display() {
+        const username = document.getElementById('uInput').value;
+        console.log(username);
+    }
+</script>
 </body>
 </html>
