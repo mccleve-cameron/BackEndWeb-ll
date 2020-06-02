6d19344b5c1d0f9dee582b33e90b38cdbf05120b
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-06-02 12:26:39 -0600

    week6

--- web/week6/login.php
+++ web/week6/login.php
@@ -2,28 +2,6 @@
     session_start();
     require "../../dbConnect.php";
     $db = getDb();
-
-    $name = 'John';
-    if (isset($_GET['uInput'])) { 
-        $_SESSION['username'] = $_GET['uInput'];
-        $name = $_SESSION['username'];
-    }
-
-    $stmt = $db->prepare("SELECT * FROM users AS u 
-                    JOIN goals AS g 
-                    ON u.id = g.user_id
-                    WHERE username=:name");
-    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-    $stmt->execute();
-    $userGoals = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    $stmt2 = $db->prepare("SELECT * FROM users AS u 
-                    JOIN habits AS h 
-                    ON u.id = h.user_id
-                    WHERE username=:name");
-    $stmt2->bindValue(':name', $name, PDO::PARAM_STR);
-    $stmt2->execute();
-    $userHabits = $stmt2->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
