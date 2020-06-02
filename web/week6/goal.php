ac85008d8d4fca763af879b34fcd865260153801
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-06-02 12:17:44 -0600

    week6

--- web/week6/goal.php
+++ web/week6/goal.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GoHabitTest</title>
+    <title>GoHabit</title>
     <link rel="stylesheet" href="goal.css">
 </head>
 <header>
