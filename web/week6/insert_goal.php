c3f28b8da07b4fc0d749694829a0455e0e0a0a1f
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-06-03 14:59:36 -0600

    week6

--- web/week6/insert_goal.php
+++ web/week6/insert_goal.php
@@ -4,10 +4,6 @@ $userId = htmlspecialchars($_POST['userId']);
 $newTask = htmlspecialchars($_POST['goalContent']);
 $addDate = $_POST['date'];
 
-echo $userId;
-echo $newTask;
-echo $addDate;
-
 session_start();
 require "../dbConnect.php";
 $db = getDb();
