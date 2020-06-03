e1294cd334e871739272453b16b12ee468a3e8f1
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-06-03 15:26:24 -0600

    week6

--- web/week6/insert_goal.php
+++ web/week6/insert_goal.php
@@ -7,18 +7,16 @@ $addDate = $_POST['date'];
 require "../dbConnect.php";
 $db = getDb();
 
-echo $userId;
-echo $newTask;
-echo $addDate;
+
 
 $stmt = $db->prepare("INSERT INTO goals VALUES (DEFAULT, :newTask, FALSE, '2020-06-02', :userId);");
 $stmt->bindValue(':newTask', $newTask, PDO::PARAM_STR);
 $stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
 $stmt->execute();
 
-// $new_page = "goal.php?id=$userId";
+$new_page = "goal.php?id=$userId";
 
-// header("Location: $new_page");
-// die();
+header("Location: $new_page");
+die();
 
 ?>
