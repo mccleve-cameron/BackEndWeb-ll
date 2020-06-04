2cc9d3b95aa1e0bafa74937115efff43de76e6dc
mccleve-cameron <39345347+mccleve-cameron@users.noreply.github.com>
2020-06-04 00:07:01 -0600

    week6

--- web/week6/complete_goal.php
+++ web/week6/complete_goal.php
@@ -4,24 +4,24 @@ $username = htmlspecialchars($_GET['username']);
 $taskId = htmlspecialchars($_GET['taskId']);
 $complete = htmlspecialchars($_GET['complete']);
 
-// echo $taskId;
-// echo $username;
-// echo $complete;
+echo $taskId;
+echo $username;
+echo $complete;
 
-require "../dbConnect.php";
-$db = getDb();
+// require "../dbConnect.php";
+// $db = getDb();
 
-$complete = !$complete;
+// $complete = !$complete;
 
-$stmt = $db->prepare("UPDATE goals SET is_complete=:complete WHERE id = :taskId;");
-$stmt->bindValue(':complete', $complete, PDO::PARAM_BOOL);
-$stmt->bindValue(':taskId', $taskId, PDO::PARAM_BOOL);
+// $stmt = $db->prepare("UPDATE goals SET is_complete=:complete WHERE id = :taskId;");
+// $stmt->bindValue(':complete', $complete, PDO::PARAM_BOOL);
+// $stmt->bindValue(':taskId', $taskId, PDO::PARAM_BOOL);
 
-$stmt->execute();
+// $stmt->execute();
 
-$new_page = "goal.php?username=$username";
+// $new_page = "goal.php?username=$username";
 
-header("Location: $new_page");
-die();
+// header("Location: $new_page");
+// die();
 
 ?>
